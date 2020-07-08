ec590805198af3be023decb48862b20a6a2908b2
DedsecHAP <49938387+DedsecHAP@users.noreply.github.com>
2020-07-08 11:03:41 +0700

    Tugas Akhir

--- Tabel.php
+++ Tabel.php
@@ -7,47 +7,55 @@
     body{
       position: relative;
       top: 20px;
-      left: 40px;
+    }
+    .container{
+      width:77%;
+      padding:40px;
+      margin-left:80px;
+      box-shadow:0 0 5px 0 black;
+      border-radius:20px;
+
     }
     table tbody tr{
       text-align: center;
+    }
+    table{
+      border-collapse:collapse;
+    }
+    table,tr,td{
+      border:1px solid black;
     }
   </style>
 </head>
 
 <body>
-	<div>
-		<table width="1024" height="232" border="1">
+	<div class="container">
+    <h1 style="margin-left:100px;">Daftar Kritik Dan Saran Mahasiswa Teknik Informatika</h1>
+		<table width="1024">
       <tbody>
-        <tr>
-          <td width="215" style="text-align: center">Nama</td>
-          <td width="134" style="text-align: center">NIM</td>
-          <td width="154" style="text-align: center">Jurusan</td>
-          <td width="51" style="text-align: center">Angkatan</td>
+        <tr style="font-weight:bold; background-color:green; color:white;">
+          <td width="35" height="40">No</td>
+          <td width="120" style="text-align: center">Nim</td>
+          <td width="200" style="text-align: center">Nama</td>
+          <td width="150" style="text-align: center">Jurusan</td>
+          <td width="80" style="text-align: center">Angkatan</td>
           <td width="54" style="text-align: center">Umur</td>
           <td width="125" style="text-align: center">Jenis Kelamin</td>
-          <td width="185" style="text-align: center">Alamat</td>
+          <td width="185" style="text-align: center">Kritik Dan Saran</td>
         </tr>
-        <?php if(isset($_GET["submit"])): ?>
-          <?php if (empty($_GET["innama"]) || empty($_GET["innim"])): ?>
-              <script type="text/javascript">
-                 var cek = window.confirm("Ada Kolom yang Kosong, Silahkan Kembali Ke Form")
-                 if (cek == true) {
-                    window.location.href = "Form.html";
-                 }
-              </script>
-            <?php else: ?>
-              <tr>
-                <td><h3><?= $_GET["innama"]; ?></h3></td>
-                <td><h3><?= $_GET["innim"]; ?></h3></td>
-                <td><h3><?= $_GET["injurusan"]; ?></h3></td>
-                <td><h3><?= $_GET["inangkatan"]; ?></h3></td>
-                <td><h3><?= $_GET["inumur"]; ?></h3></td>
-                <td><h3><?= $_GET["jenis_kelamin"]; ?></h3></td>
-                <td><h3><?= $_GET["inalamat"]; ?></h3></td>
-              </tr>
-          <?php endif ?>
-      <?php endif; ?>
+        <?php $file = fopen("kritik&saran.txt","r"); $i=1;?>
+        <?php while ($isi = fgets($file,1000)): $data = explode("|",$isi);?>
+          <tr>
+            <td><p><?= $i ?></p></td>
+            <td><p><?= $data[1] ?></p></td>
+            <td><p><?= $data[0] ?></p></td>
+            <td><p><?= $data[2] ?></p></td>
+            <td><p><?= $data[3] ?></p></td>
+            <td><p><?= $data[4] ?></p></td>
+            <td><p><?= $data[5] ?></p></td>
+            <td><p><?= $data[6] ?></p></td>
+          </tr>
+        <?php $i++; endwhile;  ?>
       </tbody>
     </table>
   </div>
